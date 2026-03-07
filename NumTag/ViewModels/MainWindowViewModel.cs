46bffbf52e3872e691816396173a73aa62c375cf
任天天 <ruattqwq@outlook.com>
2026-03-07 13:52:09 +0800

    initial commit

---
+++ NumTag/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NumTag.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ NumTag/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NumTag.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
