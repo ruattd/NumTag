dbe4961b133804f757326fb86d1b3f9723a3d4e1
任天天 <ruattqwq@outlook.com>
2026-03-08 16:10:51 +0800

    fix: adjust invalid layout parameters

--- NumTag/ViewModels/MainWindowViewModel.cs
+++ NumTag/ViewModels/MainWindowViewModel.cs
@@ -6,15 +6,16 @@ namespace NumTag.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private const long DoubleClickMaxLatency = 500;
+    private const long DoubleClickMaxLatency = 300;
 
     [ObservableProperty] private string _title = "Loading Title";
-    [ObservableProperty] private double _titleTextSize = 24;
+    [ObservableProperty] private double _titleTextSize = 96;
     [ObservableProperty] private string _subtitle = "Loading subtitle";
-    [ObservableProperty] private double _subtitleTextSize = 16;
+    [ObservableProperty] private double _subtitleTextSize = 32;
     [ObservableProperty] private string _hint = "双击空白处关闭";
-    [ObservableProperty] private double _hintTextSize = 12;
+    [ObservableProperty] private double _hintTextSize = 16;
     [ObservableProperty] private Brush _foreground = new SolidColorBrush(Colors.White);
+    [ObservableProperty] private Brush _hintForeground = new SolidColorBrush(Colors.LightGray);
     [ObservableProperty] private Brush _background = new SolidColorBrush(Colors.Black);
     [ObservableProperty] private bool _windowVisible = false;
 
