3cecb70eca2e70e1e708c5a3c18050da0edcbc6f
任天天 <ruattqwq@gmail.com>
2026-03-08 23:40:49 +0800

    feat(settings): support solid color / linear gradient brushs

--- NumTag/Models/BrushOption.cs
+++ NumTag/Models/BrushOption.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Avalonia;
 using Avalonia.Media;
 
 namespace NumTag.Models;
@@ -6,13 +10,72 @@ namespace NumTag.Models;
 public enum BrushKind
 {
     SolidColor,
-    GradientColor,
+    LinearGradient,
 }
 
 public interface IBrushOptionParser
 {
     public Brush Decode(string content);
     public string Encode(Brush brush);
+}
+
+file sealed class SolidColorBrushParser : IBrushOptionParser
+{
+    private SolidColorBrushParser() {}
+    public static readonly SolidColorBrushParser Instance = new();
+
+    public Brush Decode(string content)
+    {
+        var color = Color.Parse(content);
+        return new SolidColorBrush(color);
+    }
+
+    public string Encode(Brush brush)
+    {
+        return brush is SolidColorBrush solid ? solid.Color.ToString() : throw new InvalidOperationException();
+    }
+}
+
+file sealed class LinearGradientBrushParser : IBrushOptionParser
+{
+    private LinearGradientBrushParser() {}
+    public static readonly LinearGradientBrushParser Instance = new();
+
+    public Brush Decode(string content)
+    {
+        var parts = content.Split(':');
+        var startPoint = RelativePoint.Parse(parts[0]);
+        var endPoint = RelativePoint.Parse(parts[1]);
+        Enum.TryParse<GradientSpreadMethod>(parts[2], true, out var spreadMethod);
+        var stopParts = parts.Length > 3 ? parts[3].Split(';') : [];
+        var stops = new GradientStops();
+        foreach (var part in stopParts)
+        {
+            var parts2 = part.Split(',', 2);
+            var offset = double.Parse(parts2[0]);
+            var color = Color.Parse(parts2[1]);
+            stops.Add(new GradientStop(color, offset));
+        }
+        return new LinearGradientBrush
+        {
+            StartPoint = startPoint,
+            EndPoint = endPoint,
+            SpreadMethod = spreadMethod,
+            GradientStops = stops
+        };
+    }
+
+    public string Encode(Brush brush)
+    {
+        if (brush is not LinearGradientBrush gradient) throw new InvalidOperationException();
+        var sb = new StringBuilder();
+        sb.Append(gradient.StartPoint).Append(':').Append(gradient.EndPoint).Append(':');
+        sb.Append(gradient.SpreadMethod.ToString().ToLowerInvariant()).Append(':');
+        foreach (var stop in gradient.GradientStops)
+            sb.Append(stop.Offset).Append(',').Append(stop.Color).Append(';');
+        sb.Remove(sb.Length - 1, 1);
+        return sb.ToString();
+    }
 }
 
 public record BrushOption
@@ -22,6 +85,8 @@ public record BrushOption
 
     private static IBrushOptionParser GetParser(BrushKind kind) => kind switch
     {
+        BrushKind.SolidColor => SolidColorBrushParser.Instance,
+        BrushKind.LinearGradient => LinearGradientBrushParser.Instance,
         _ => throw new NotSupportedException($"Unsupported brush kind: {kind}"),
     };
 
@@ -29,7 +94,7 @@ public record BrushOption
     {
         BrushKind kind;
         if (brush is SolidColorBrush) kind = BrushKind.SolidColor;
-        else if (brush is GradientBrush) kind = BrushKind.GradientColor;
+        else if (brush is GradientBrush) kind = BrushKind.LinearGradient;
         else throw new NotSupportedException($"Unsupported brush type: {brush.GetType().FullName}");
         return kind;
     }
