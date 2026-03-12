ea714b88a2348c56e70532d3499fb9f9ba6faf13
任天天 <ruattqwq@gmail.com>
2026-03-12 19:31:25 +0800

    chore(ci): use JS packer instead of cli
    [skip ci]

---
+++ tools/pack.js
@@ -0,0 +1,26 @@
+﻿import generateEvb from 'generate-evb';
+import evb from 'enigmavirtualbox';
+import * as FS from "fs";
+import * as Path from 'path'
+
+console.log("Combining paths...");
+
+let dir = process.argv[2];
+let input_exe = process.argv[3];
+let output_exe = process.argv[4];
+
+let proj_path = Path.resolve(dir, "app.evb")
+let input_path = Path.resolve(dir, input_exe);
+let output_path = Path.resolve(dir, output_exe);
+
+console.log("Generating EVB project file...");
+
+generateEvb(proj_path, input_path, output_path, dir, {
+    filter: (fullPath, name, isDir) => name !== input_exe
+});
+
+console.log("Creating executable bundle...")
+
+await evb.cli(proj_path);
+
+console.log("Success!")
