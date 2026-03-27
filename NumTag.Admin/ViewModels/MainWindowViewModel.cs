d57075de32a1d3e028c76c2c82bf5e931c5c5883
任天天 <ruattqwq@gmail.com>
2026-03-27 16:41:51 +0800

    feat: initialize admin system

---
+++ NumTag.Admin/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using NumTag.Core.ViewModels;
+
+namespace NumTag.Admin.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
